deb813a3bb1b9a89fe304d8d60b9132d3f148af1
ilhamdoanggg <ilhamdoanggg@gmail.com>
2017-09-03 13:26:28 +0700

    add KondisiOpertor

--- src/KondisiOperator.java
+++ src/KondisiOperator.java
@@ -1,11 +1,19 @@
 public class KondisiOperator {
     public static void main(String[] args) {
-        String status ="";
+        /*String status ="";
         int grade = 80;
 
         status=(grade >= 60)?"Passed":"Fail";
 
         System.out.println(status);
+*/
 
+
+        int score = 3;
+        char answer = 'a';
+
+        score = (answer == 'a')? 10:0;
+
+        System.out.println("score = " + score);
     }
 }
