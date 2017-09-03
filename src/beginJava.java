5a2e452b7f913e303d7664b86b70d9700dc64e05
ilhamdoanggg <ilhamdoanggg@gmail.com>
2017-09-03 12:54:47 +0700

    add some future

---
+++ src/beginJava.java
@@ -0,0 +1,50 @@
+public class beginJava {
+    public static void main(String[] args) {
+        /*deklarasi variable yang ada
+        * */
+        int i = 37;
+        int j = 42;
+        int k = 42;
+
+        System.out.println("Nilai Variable");
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+
+        System.out.println("Lebih Besar Dari");
+        System.out.println("i > j " + (i > j));
+        System.out.println("j > i " + (j > i));
+        System.out.println("k > j " + (k > j));
+
+        System.out.println("Lebih Besar Dari atau sama dengan");
+        System.out.println("i >= j " + (i >= j));
+        System.out.println("j >= i " + (j >= i));
+        System.out.println("k >= j " + (k >= j));
+//
+//        System.out.println("Lebih Besar Dari atau sama dengan");
+//        System.out.println("i > j "+ (i>j));
+//        System.out.println("j > i "+ (j>i));
+//        System.out.println("k > j "+ (k>j));
+
+        System.out.println("Lebih kecil Dari");
+        System.out.println("i < j " + (i < j));
+        System.out.println("j < i " + (j < i));
+        System.out.println("k < j " + (k < j));
+
+        System.out.println("Lebih keci Dari atau sama dengan");
+        System.out.println("i <= j " + (i <= j));
+        System.out.println("j <= i " + (j <= i));
+        System.out.println("k <= j " + (k <= j));
+
+        System.out.println("sama dengan");
+        System.out.println("i == j " + (i == j));
+        System.out.println("j == i " + (j == i));
+        System.out.println("k == j " + (k == j));
+
+        System.out.println("tidak sama dengan");
+        System.out.println("i != j " + (i != j));
+        System.out.println("j != i " + (j != i));
+        System.out.println("k != j " + (k != j));
+
+    }
+}
