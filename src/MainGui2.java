534d6ccade9cb9e04d24ec210bc66ea22455aeea
ilhamdoanggg <ilhamdoanggg@gmail.com>
2017-09-04 20:29:52 +0700

    mencoba java GUI

--- src/MainGui2.java
+++ src/MainGui2.java
@@ -1,2 +1,17 @@
+import java.awt.*;
+
 public class MainGui2 {
+    public static void main(String[] args) {
+        Frame f = new Frame("Tutorial Mangan");
+        Component text = new TextArea("mangan Karu Uyah");
+        Component button = new Button("Tombol");
+
+        f.add(text, BorderLayout.NORTH);
+        f.add(text, BorderLayout.SOUTH);
+        int width = 300;
+        int height = 300;
+        f.setSize(width, height);
+        f.setVisible(true);
+
+    }
 }
