c545e5eee6405b6f06ba46cd1e0a90de22aebd82
ilhamdoanggg <ilhamdoanggg@gmail.com>
2017-09-04 22:17:00 +0700

    add gui mode on

---
+++ src/MaenPanel2.java
@@ -0,0 +1,21 @@
+import java.applet.*;
+import javax.swing.*;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+import java.text.*;
+import javax.swing.*;
+import com.ilham.*;
+
+
+public class MaenPanel2 extends JPanel {
+
+    @Override
+    public void setVerifyInputWhenFocusTarget(boolean verifyInputWhenFocusTarget) {
+        super.setVerifyInputWhenFocusTarget(verifyInputWhenFocusTarget);
+    }
+    public static void main(String[] args) {
+
+    }
+}
